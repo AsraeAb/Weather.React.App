82615a24d9d55b6108ce2f2b80bf4b5d024fffa0
AsraeAb <133543221+AsraeAb@users.noreply.github.com>
2023-06-18 18:15:45 +0330

    added features

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import Weather from "./Weather";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Weather Forecast</h1>
+        <Weather />
       </header>
+      <footer>
+        {" "}
+        <a
+          class="git-link"
+          href="https://github.com/AsraeAb/Vanila.Weather.git"
+        >
+          Open-source code
+        </a>{" "}
+        <span class="span"> By Asrae</span>{" "}
+      </footer>
     </div>
   );
 }
